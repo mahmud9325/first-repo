cdf57b441d12d77a3dfa708856b42f0e9d47c9b0
mahmud2593 <mahmudulhoque93251@gmail.com>
2025-11-24 22:21:34 +0600

    first commit

---
+++ js/project08.js
@@ -0,0 +1,18 @@
+const calculatorTip = () => {
+    const billAmount = parseFloat(document.getElementById('billAmount').value);
+    const tipPercentage = parseFloat(document.getElementById('tipPercentage').value);
+
+    if (isNaN(billAmount) || isNaN(tipPercentage)) {
+        document.getElementById('result').textContent = 'Please Enter The value';
+        return;
+    }
+
+    const tipAmount = (billAmount * tipPercentage) / 100;
+    const totalAmount = billAmount + tipAmount;
+
+    document.getElementById('result').innerHTML = `
+      <p>Tip Amount: $${tipAmount.toFixed(2)}</p>
+      <p>Total Amount: $${totalAmount.toFixed(2)}</p>
+    `;
+
+}
